d1c546d38d742898c966a43b429640d2c6ca5aa6
shadejinx <github.shadejinx.0gqn5@slmail.me>
2024-02-18 11:23:50 -0800

    Added LogScale specific commands to the backend

--- sigma/backends/logscale/logscale.py
+++ sigma/backends/logscale/logscale.py
@@ -36,7 +36,7 @@ class logscaleBackend(TextQueryBackend):
     # String output
     ## Fields
     ### Quoting
-    field_quote : ClassVar[str] = "'"                               # Character used to quote field characters if field_quote_pattern matches (or not, depending on field_quote_pattern_negation). No field name quoting is done if not set.
+    field_quote : ClassVar[str] = '"'                               # Character used to quote field characters if field_quote_pattern matches (or not, depending on field_quote_pattern_negation). No field name quoting is done if not set.
     field_quote_pattern : ClassVar[Pattern] = re.compile("^\\w+$")   # Quote field names if this pattern (doesn't) matches, depending on field_quote_pattern_negation. Field name is always quoted if pattern is not set.
     field_quote_pattern_negation : ClassVar[bool] = True            # Negate field_quote_pattern result. Field name is quoted if pattern doesn't matches if set to True (default).
 
@@ -58,16 +58,16 @@ class logscaleBackend(TextQueryBackend):
     }
 
     # String matching operators. if none is appropriate eq_token is used.
-    startswith_expression : ClassVar[str] = "{field} startswith {value}"
-    endswith_expression   : ClassVar[str] = "{field} endswith {value}"
-    contains_expression   : ClassVar[str] = "{field} contains {value}"
-    wildcard_match_expression : ClassVar[str] = "{field} match {value}"      # Special expression if wildcards can't be matched with the eq_token operator
+    startswith_expression : ClassVar[str] = "{field} = {value}*"
+    endswith_expression   : ClassVar[str] = "{field} = *{value}"
+    contains_expression   : ClassVar[str] = "{field} *{value}*"
+    wildcard_match_expression : ClassVar[str] = None ### "{field} match {value}"      # Special expression if wildcards can't be matched with the eq_token operator
 
     # Regular expressions
     # Regular expression query as format string with placeholders {field}, {regex}, {flag_x} where x
     # is one of the flags shortcuts supported by Sigma (currently i, m and s) and refers to the
     # token stored in the class variable re_flags.
-    re_expression : ClassVar[str] = "{field}=~{regex}"
+    re_expression : ClassVar[str] = "{field}=/{regex}/{flag_x}"
     re_escape_char : ClassVar[str] = "\\"               # Character used for escaping in regular expressions
     re_escape : ClassVar[Tuple[str]] = ()               # List of strings that are escaped
     re_escape_escape_char : bool = True                 # If True, the escape character is also escaped
@@ -79,17 +79,17 @@ class logscaleBackend(TextQueryBackend):
     re_flags : Dict[SigmaRegularExpressionFlag, str] = {
         SigmaRegularExpressionFlag.IGNORECASE: "i",
         SigmaRegularExpressionFlag.MULTILINE : "m",
-        SigmaRegularExpressionFlag.DOTALL    : "s",
+        SigmaRegularExpressionFlag.DOTALL    : "d",
     }
 
     # Case sensitive string matching expression. String is quoted/escaped like a normal string.
     # Placeholders {field} and {value} are replaced with field name and quoted/escaped string.
-    case_sensitive_match_expression : ClassVar[str] = "{field} casematch {value}"
+    case_sensitive_match_expression : ClassVar[str] = None ### "{field} casematch {value}"
     # Case sensitive string matching operators similar to standard string matching. If not provided,
     # case_sensitive_match_expression is used.
-    case_sensitive_startswith_expression : ClassVar[str] = "{field} casematch_startswith {value}"
-    case_sensitive_endswith_expression   : ClassVar[str] = "{field} casematch_endswith {value}"
-    case_sensitive_contains_expression   : ClassVar[str] = "{field} casematch_contains {value}"
+    case_sensitive_startswith_expression : ClassVar[str] = "{field} = {value}*"
+    case_sensitive_endswith_expression   : ClassVar[str] = "{field} = *{value}"
+    case_sensitive_contains_expression   : ClassVar[str] = "{field} = *{value}*"
 
     # CIDR expressions: define CIDR matching if backend has native support. Else pySigma expands
     # CIDR values into string wildcard matches.
@@ -106,21 +106,21 @@ class logscaleBackend(TextQueryBackend):
     }
 
     # Expression for comparing two event fields
-    field_equals_field_expression : ClassVar[Optional[str]] = None  # Field comparison expression with the placeholders {field1} and {field2} corresponding to left field and right value side of Sigma detection item
+    field_equals_field_expression : ClassVar[Optional[str]] = "test({field1} == {field2})"  # Field comparison expression with the placeholders {field1} and {field2} corresponding to left field and right value side of Sigma detection item
     field_equals_field_escaping_quoting : Tuple[bool, bool] = (True, True)   # If regular field-escaping/quoting is applied to field1 and field2. A custom escaping/quoting can be implemented in the convert_condition_field_eq_field_escape_and_quote method.
 
     # Null/None expressions
-    field_null_expression : ClassVar[str] = "{field} is null"          # Expression for field has null value as format string with {field} placeholder for field name
+    field_null_expression : ClassVar[str] = "{field} != *"          # Expression for field has null value as format string with {field} placeholder for field name
 
     # Field existence condition expressions.
-    field_exists_expression : ClassVar[str] = "exists({field})"             # Expression for field existence as format string with {field} placeholder for field name
-    field_not_exists_expression : ClassVar[str] = "notexists({field})"      # Expression for field non-existence as format string with {field} placeholder for field name. If not set, field_exists_expression is negated with boolean NOT.
+    field_exists_expression : ClassVar[str] = "{field} = *"             # Expression for field existence as format string with {field} placeholder for field name
+    field_not_exists_expression : ClassVar[str] = "{field} != *"      # Expression for field non-existence as format string with {field} placeholder for field name. If not set, field_exists_expression is negated with boolean NOT.
 
     # Field value in list, e.g. "field in (value list)" or "field containsall (value list)"
     convert_or_as_in : ClassVar[bool] = True                     # Convert OR as in-expression
-    convert_and_as_in : ClassVar[bool] = True                    # Convert AND as in-expression
-    in_expressions_allow_wildcards : ClassVar[bool] = True       # Values in list can contain wildcards. If set to False (default) only plain values are converted into in-expressions.
-    field_in_list_expression : ClassVar[str] = "{field} {op} ({list})"  # Expression for field in list of values as format string with placeholders {field}, {op} and {list}
+    convert_and_as_in : ClassVar[bool] = False                    # Convert AND as in-expression
+    in_expressions_allow_wildcards : ClassVar[bool] = False       # Values in list can contain wildcards. If set to False (default) only plain values are converted into in-expressions.
+    field_in_list_expression : ClassVar[str] = "in(field={field}, value=[{list}])"  # Expression for field in list of values as format string with placeholders {field}, {op} and {list}
     or_in_operator : ClassVar[str] = "in"               # Operator used to convert OR into in-expressions. Must be set if convert_or_as_in is set
     and_in_operator : ClassVar[str] = "contains-all"    # Operator used to convert AND into in-expressions. Must be set if convert_and_as_in is set
     list_separator : ClassVar[str] = ", "               # List element separator
@@ -128,7 +128,7 @@ class logscaleBackend(TextQueryBackend):
     # Value not bound to a field
     unbound_value_str_expression : ClassVar[str] = '"{value}"'   # Expression for string value not bound to a field as format string with placeholder {value}
     unbound_value_num_expression : ClassVar[str] = '{value}'     # Expression for number value not bound to a field as format string with placeholder {value}
-    unbound_value_re_expression : ClassVar[str] = '_=~{value}'   # Expression for regular expression not bound to a field as format string with placeholder {value} and {flag_x} as described for re_expression
+    unbound_value_re_expression : ClassVar[str] = '/{value}/{flag_x}'   # Expression for regular expression not bound to a field as format string with placeholder {value} and {flag_x} as described for re_expression
 
     # Query finalization: appending and concatenating deferred query part
     deferred_start : ClassVar[str] = "\n| "               # String used as separator between main query and deferred parts
